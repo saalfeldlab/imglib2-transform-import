899e44220f29a3ae988f74f8e48f0639c5af609d
John Bogovic <bogovicj@janelia.hhmi.org>
2017-03-24 15:53:21 -0400

    Fix bioformats problems in reading ants deformation fields

--- src/main/java/io/nii/NiftiIo.java
+++ src/main/java/io/nii/NiftiIo.java
@@ -5,8 +5,14 @@ import java.io.IOException;
 import java.util.Hashtable;
 
 import loci.formats.FormatException;
+import loci.formats.in.DefaultMetadataOptions;
+import loci.formats.in.MetadataLevel;
 import loci.formats.in.NiftiReader;
 import loci.plugins.util.ImageProcessorReader;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.util.Util;
+import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.process.ImageProcessor;
@@ -17,12 +23,11 @@ public class NiftiIo
 	public static final String KEY_YRES = "Voxel height";
 	public static final String KEY_ZRES = "Slice thickness";
 	
-	
 	public static ImagePlus readNifti( File f ) throws FormatException, IOException
 	{
-		NiftiReader reader = new NiftiReader();
+		AntsDefNiftiReader reader = new AntsDefNiftiReader();
+		reader.setMetadataOptions( new DefaultMetadataOptions( MetadataLevel.MINIMUM ));
 		reader.setId( f.getAbsolutePath() );
-		
 
 		int width = reader.getSizeX();
 		int height = reader.getSizeY();
@@ -32,6 +37,7 @@ public class NiftiIo
 		int imgCount = reader.getImageCount();
 		System.out.println( "img count" + imgCount );
 		System.out.println("timepts " + timepts  );
+		System.out.println("depth " + depth  );
 		System.out.println("channels " + channels  );
 		
 		if( timepts > 1 && channels == 1 )
@@ -41,11 +47,6 @@ public class NiftiIo
 		
 		double[] res = new double[]{ 1.0, 1.0, 1.0 };
 		Hashtable< String, Object > meta = reader.getGlobalMetadata();
-//		for( String key : meta.keySet() )
-//		{
-//			System.out.println( " " + key + " - " + meta.get( key ));
-////			System.out.println( key.equals( KEY_XRES ));
-//		}
 
 		// Get image resolutions from file
 		if( meta.keySet().contains( KEY_XRES ))
@@ -66,17 +67,11 @@ public class NiftiIo
 			res[ 2 ] = (Double)(meta.get( KEY_ZRES ));
 		}
 		
-		//System.out.println( "res: " + res[0]  + " " + res[1] + " " + res[2] );
-
 		ImageProcessorReader ipr = new ImageProcessorReader( reader );
-//		ipr.openProcessors( arg0, arg1, arg2, arg3, arg4 )
 		ImageStack stack = new ImageStack( width, height );
 		for ( int z = 0; z < imgCount; z++ )
 		{
-//			System.out.println( "z: " + z );
 			ImageProcessor[] processors = ipr.openProcessors( z );
-//			System.out.println( processors.length );
-//			System.out.println( processors[ 0 ] );
 			stack.addSlice( processors[ 0 ] );
 		}
 
@@ -84,6 +79,7 @@ public class NiftiIo
 		ipr.close();
 
 		ImagePlus ip = new ImagePlus( f.getName(), stack );
+		//switching channels and frames for imglib2 reasons
 		ip.setDimensions( 1, depth, channels );
 
 		ip.getCalibration().pixelWidth  = res[ 0 ];
@@ -91,5 +87,26 @@ public class NiftiIo
 		ip.getCalibration().pixelDepth  = res[ 2 ];
 
 		return ip;
+	
+	}
+	
+	public static void main( String[] args ) throws FormatException, IOException
+	{
+//		String niipath = "/groups/saalfeld/home/bogovicj/projects/flyChemStainAtlas/ants_groundTruth/groupwise_all_884/ALL-F-A1_TileConfiguration_lensrepaired.nii.gz";
+		//String niipath = "/groups/saalfeld/home/bogovicj/projects/flyChemStainAtlas/ants_groundTruth/NEW_groupwise_all-flip_884/ALLF-M-A4_TileConfiguration_lensWarp-mipav.nii.gz";
+		//String niipath = "/nrs/saalfeld/john/projects/flyChemStainAtlas/take5_groupwise_template_442/all-flip/ALLF-M-I2_TileConfiguration_lens_registered_down442Warp.nii.gz";
+		String niipath = "/data-ssd/john/ALLF-F-A1_TileConfiguration_lens.registered_down_flipHRWarp.nii";
+		ImagePlus ip = readNifti( new File( niipath ));
+//		ImagePlus ip = myReadNifti( new File( niipath ));
+		System.out.println( ip );
+		IJ.save( ip, "/data-ssd/john/ALLF-F-A1.tif" );
+		
+		Img<?> img = ImageJFunctions.wrap( ip );
+		
+		System.out.println( Util.printInterval( img ));
+		
+//		ImagePlus[] ip = BF.openImagePlus( niipath );
+//		System.out.println( ip );
+//		System.out.println( ip.length );
 	}
 }
