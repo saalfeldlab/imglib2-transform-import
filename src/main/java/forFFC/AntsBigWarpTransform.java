9f9c7745cb821b0fee02196fc6a3d0a418793b17
John Bogovic <bogovicj@janelia.hhmi.org>
2017-04-14 13:32:39 -0400

    multithread the export

--- src/main/java/forFFC/AntsBigWarpTransform.java
+++ src/main/java/forFFC/AntsBigWarpTransform.java
@@ -2,6 +2,12 @@ package forFFC;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import bdv.img.TpsTransformWrapper;
 import bigwarp.landmarks.LandmarkTableModel;
@@ -10,15 +16,26 @@ import ij.IJ;
 import ij.ImagePlus;
 import io.nii.NiftiIo;
 import loci.formats.FormatException;
+import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealRandomAccessible;
+import net.imglib2.exception.ImgLibException;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.img.imageplus.FloatImagePlus;
+import net.imglib2.img.imageplus.ImagePlusImgs;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.realtransform.InvertibleRealTransformSequence;
 import net.imglib2.realtransform.RealViews;
 import net.imglib2.realtransform.ants.ANTSDeformationField;
 import net.imglib2.realtransform.ants.ANTSLoadAffine;
+import net.imglib2.type.numeric.NumericType;
 import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
@@ -49,7 +66,7 @@ public class AntsBigWarpTransform
 		/nobackup/saalfeld/john/wong_alignment/brains_2016Mar/groupwiseRegSpacing/MY20160301_r3_ss2527lc10_frup65_flya_00001_baseline_spacingWarp_mipav.nii
 	 */
 	
-	public static void main( String[] args ) throws FormatException, IOException
+	public static void main( String[] args ) throws FormatException, IOException, ImgLibException
 	{
 		System.out.println("Started AntsBigWarpTransform111");
 		File srcF  = new File( args[0] );
@@ -66,6 +83,9 @@ public class AntsBigWarpTransform
 		if( args.length > 5 )
 			defF = new File( args[5] );
 
+		int nThreads = 1;
+		if( args.length > 6 )
+			nThreads = Integer.parseInt( args[6] );
 
 		/* 
 		 * READ THE IMAGE
@@ -125,9 +145,13 @@ public class AntsBigWarpTransform
 		IntervalView< FloatType > output = Views.interval( Views.raster( 
 				 RealViews.transform( imgrra, totalXfm )), 
 				 interval);
+
+		FloatImagePlus< FloatType > ipimg = ImagePlusImgs.floats( Intervals.dimensionsAsLongArray( interval ) );
+		System.out.println("copyting with " + nThreads + " threads.");
+		copyToImageStack( output, ipimg, nThreads );
 		
 		System.out.println( "writing" );
-		ImagePlus ipout = ImageJFunctions.wrap( output, "xfm" );
+		ImagePlus ipout = ipimg.getImagePlus();
 		ipout.getCalibration().pixelWidth = itvl.getCalibration().pixelWidth;
 		ipout.getCalibration().pixelHeight = itvl.getCalibration().pixelHeight;
 		ipout.getCalibration().pixelDepth = itvl.getCalibration().pixelDepth;
@@ -147,4 +171,108 @@ public class AntsBigWarpTransform
 		return ip;
 	}
 	
+	public static < T extends NumericType<T> > RandomAccessibleInterval<T> copyToImageStack( 
+			final RandomAccessible< T > ra,
+			final RandomAccessibleInterval<T> target,
+			final int nThreads )
+	{
+		// TODO I wish I didn't have to do this inside this method
+//		MixedTransformView< T > raible = Views.permute( ra, 2, 3 );
+		RandomAccessible< T > raible = ra;
+
+		// what dimension should we split across?
+		int nd = raible.numDimensions();
+		int tmp = nd - 1;
+		while( tmp >= 0 )
+		{
+			if( target.dimension( tmp ) > 1 )
+				break;
+			else
+				tmp--;
+		}
+		final int dim2split = tmp;
+
+		final long[] splitPoints = new long[ nThreads + 1 ];
+		long N = target.dimension( dim2split );
+		long del = ( long )( N / nThreads ); 
+		splitPoints[ 0 ] = target.min( dim2split );
+		splitPoints[ nThreads ] = target.max( dim2split ) + 1;
+		for( int i = 1; i < nThreads; i++ )
+		{
+			splitPoints[ i ] = splitPoints[ i - 1 ] + del;
+			System.out.println( "splitPoints[i]: " + splitPoints[ i ] ); 
+		}
+//		System.out.println( "dim2split: " + dim2split );
+//		System.out.println( "split points: " + XfmUtils.printArray( splitPoints ));
+
+		ExecutorService threadPool = Executors.newFixedThreadPool( nThreads );
+
+		LinkedList<Callable<Boolean>> jobs = new LinkedList<Callable<Boolean>>();
+		for( int i = 0; i < nThreads; i++ )
+		{
+			final long start = splitPoints[ i ];
+			final long end   = splitPoints[ i+1 ];
+
+			jobs.add( new Callable<Boolean>()
+			{
+				public Boolean call()
+				{
+					try
+					{
+						final FinalInterval subItvl = getSubInterval( target, dim2split, start, end );
+						final IntervalView< T > subTgt = Views.interval( target, subItvl );
+						final Cursor< T > c = subTgt.cursor();
+						final RandomAccess< T > ra = raible.randomAccess();
+						while ( c.hasNext() )
+						{
+							c.fwd();
+							ra.setPosition( c );
+							c.get().set( ra.get() );
+						}
+						return true;
+					}
+					catch( Exception e )
+					{
+						e.printStackTrace();
+					}
+					return false;
+				}
+			});
+		}
+		try
+		{
+			List< Future< Boolean > > futures = threadPool.invokeAll( jobs );
+			threadPool.shutdown(); // wait for all jobs to finish
+
+		}
+		catch ( InterruptedException e1 )
+		{
+			e1.printStackTrace();
+		}
+
+		IJ.showProgress( 1.1 );
+		return target;
+	}
+	
+	public static FinalInterval getSubInterval( Interval interval, int d, long start, long end )
+	{
+		int nd = interval.numDimensions();
+		long[] min = new long[ nd ];
+		long[] max = new long[ nd ];
+		for( int i = 0; i < nd; i++ )
+		{
+			if( i == d )
+			{
+				min[ i ] = start;
+				max[ i ] = end - 1;
+			}
+			else
+			{
+				min[ i ] = interval.min( i );
+				max[ i ] = interval.max( i );
+			}
+		}
+		return new FinalInterval( min, max );
+	}
+
 }
